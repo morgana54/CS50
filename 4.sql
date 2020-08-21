1437505c0a724b46a2af5defa19c1c166e435e3e
morgana54 <morgana54@users.noreply.github.com>
2020-08-21 14:16:59 +0000

    automated commit by submit50

--- 4.sql
+++ 4.sql
@@ -1,1 +1,3 @@
-SELECT COUNT(title) FROM movies WHERE (SELECT rating FROM ratings WHERE rating = 10.0);
+SELECT COUNT(title) FROM movies
+JOIN ratings ON movies.id = ratings.movie_id
+WHERE rating = 10.0;
